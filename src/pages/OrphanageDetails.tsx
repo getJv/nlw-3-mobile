e39783400f043d17ee07df8c50cefdbe1e9ec973
Jhonatan Vinicius Paulino de Morais <jhonatan.morais@epl.gov.br>
2020-10-15 18:19:02 -0300

    nlw3 day 4

---
+++ src/pages/OrphanageDetails.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, Text, View,  } from 'react-native';
+import { Feather } from '@expo/vector-icons'
+
+export default function OrphanageDetails() { 
+    return (
+    <View style={styles.container}>
+      <Text>Details page</Text>
+    </View>
+    )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  
+});
